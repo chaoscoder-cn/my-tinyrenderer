b5bd2074d35e1b10d920185505289502947eed0a
chaoscoder-cn <colordream2023@gmail.com>
2025-02-17 00:59:42 +0800

    feat：添加tgaimage头文件，初始化项目

---
+++ myTinyRenderer/src/main.cpp
@@ -0,0 +1,10 @@
+#include "tgaimage.h"
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
+int main()
+{
+	TGAImage image(100, 100, TGAImage::RGBA);
+	image.set(52, 41, red);
+	image.write_tga_file("..\\exe\\output.tga");
+	return 0;
+}
