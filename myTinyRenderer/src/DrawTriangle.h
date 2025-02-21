578e9c8cf1515a954c1bbc139c481dc604b6e8bb
chaoscoder-cn <colordream2023@gmail.com>
2025-02-21 23:49:38 +0800

    feat：实现绘制面的算法，以及简单的根据光照来绘制面

---
+++ myTinyRenderer/src/DrawTriangle.h
@@ -0,0 +1,11 @@
+#include "DrawLine.h"
+#include "tgaimage.h"
+#include "geometry.h"
+
+class DrawTriangle
+{
+public:
+	DrawTriangle() = default;
+	//…®√Ëœﬂ∑®
+	void DrawTriangleBySimple1(Vec2i p0, Vec2i p1, Vec2i p2, TGAImage& image, TGAColor& color);
+};
--- myTinyRenderer/src/main.cpp
+++ myTinyRenderer/src/main.cpp
@@ -1,43 +1,91 @@
 #include "tgaimage.h"
 #include "Log.h"
-#include "Chapter_One.h"
+#include "DrawLine.h"
+#include "DrawTriangle.h"
 #include "model.h"
 
+TGAColor white(255, 255, 255, 255);
+TGAColor red(255, 0, 0, 255);
+TGAColor green(0, 255, 0, 255);
+
+const int width = 1000;
+const int height = 1000;
 int main()
 {
 
-	Chapter_One chapterOne;
-	TGAImage tgaImage(1000, 1000, TGAImage::RGB);
-	TGAColor drawColor(255, 0, 0, 255);
+	TGAImage tgaImage(width, height, TGAImage::RGB);
+	DrawTriangle drawTriangle;
 
 	std::string obj_path = "african_head.obj";
 	Model model(obj_path.data());
 
-	for(int i=0;i<model.nfaces();i++)
+	Vec3f light_dir(0, 0, -1);
+	for (int i = 0; i < model.nfaces(); i++)
 	{
 		std::vector<int>face_i = model.face(i);
-		for(int j=0;j<face_i.size();j++)
+		Vec2i p[3];
+		Vec3f w[3];
+		for(int j=0;j<3;j++)
 		{
-			Vec3f vert_0 = model.vert(face_i[j]);
-			Vec3f vert_1 = model.vert(face_i[(j + 1) % face_i.size()]);
-			int x0 = (vert_0.x + 1.0f) * 500;
-			int y0 = (vert_0.y + 1.0f) * 500;
-			int x1 = (vert_1.x + 1.0f) * 500;
-			int y1 = (vert_1.y + 1.0f) * 500;
-			chapterOne.DrawLine(x0, y0, x1, y1, tgaImage, drawColor);
+			float p_x = model.vert(face_i[j]).x;
+			float p_y = model.vert(face_i[j]).y;
+			p[j] = Vec2i{ static_cast<int>((p_x + 1) / 2 * width),static_cast<int>((p_y + 1) / 2 * height) };
+			w[j] = model.vert(face_i[j]);
+		}
+		Vec3f n = (w[2] - w[0]) ^ ((w[1] - w[0]));
+		n.normalize();
+		float intensity = n * light_dir;
+		if (intensity > 0.f)
+		{
+			TGAColor drawColor(255 * intensity, 255 * intensity, 255 * intensity, 255 * intensity);
+			drawTriangle.DrawTriangleBySimple1(p[0], p[1], p[2], tgaImage, drawColor);
 		}
 	}
 
 	tgaImage.flip_vertically();
 	tgaImage.write_tga_file("obj.tga");
 
+	//直接绘制obj的所有面
+	/*TGAImage tgaImage(width, height, TGAImage::RGB);
+	DrawTriangle drawTriangle;
 
-	/*Chapter_One chapterOne;
-	chapterOne.FirstTryDrawLine();
-	chapterOne.SecondTryDrawLine();
-	chapterOne.ThirdTryDrawLine();
-	chapterOne.FourthTryDrawLine();
-	chapterOne.FifthTryDrawLine();*/
+	std::string obj_path = "african_head.obj";
+	Model model(obj_path.data());
 
+	Vec3f light_dir(0, 0, -1);
+	for (int i = 0; i < model.nfaces(); i++)
+	{
+		std::vector<int>face_i = model.face(i);
+		Vec2i p[3];
+		Vec3f w[3];
+		for (int j = 0; j < 3; j++)
+		{
+			float p_x = model.vert(face_i[j]).x;
+			float p_y = model.vert(face_i[j]).y;
+			p[j] = Vec2i{ static_cast<int>((p_x + 1) / 2 * width),static_cast<int>((p_y + 1) / 2 * height) };
+		}
+
+		drawTriangle.DrawTriangleBySimple1(p[0], p[1], p[2], tgaImage, white);
+
+	}
+
+	tgaImage.flip_vertically();
+	tgaImage.write_tga_file("obj_1.tga");*/
+
+
+
+	
+	//绘出来的三个三角形
+	/*Vec2i t0[3] = { Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80) };
+	Vec2i t1[3] = { Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180) };
+	Vec2i t2[3] = { Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180) };
+
+	TGAImage image(width, height, TGAImage::RGB);
+	DrawTriangle drawTriangle;
+	drawTriangle.DrawTriangleBySimple1(t0[0], t0[1], t0[2], image, red);
+	drawTriangle.DrawTriangleBySimple1(t1[0], t1[1], t1[2], image, white);
+	drawTriangle.DrawTriangleBySimple1(t2[0], t2[1], t2[2], image, green);
+
+	image.write_tga_file("triangle.tga");*/
 	return 0;
 }
