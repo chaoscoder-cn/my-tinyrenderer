c598724333c2e28425016a5bb220db0234ed8785
chaoscoder-cn <colordream2023@gmail.com>
2025-02-25 00:32:09 +0800

    feat：绘制面时，进行深度比对

--- myTinyRenderer/src/DrawTriangle.h
+++ myTinyRenderer/src/DrawTriangle.h
@@ -9,3 +9,15 @@ public:
 	//…®√Ëœﬂ∑®
 	void DrawTriangleBySimple1(Vec2i p0, Vec2i p1, Vec2i p2, TGAImage& image, TGAColor& color);
 };
+
+class DrawTriangleByDepth
+{
+
+public:
+	DrawTriangleByDepth(int image_width,int image_height);
+	~DrawTriangleByDepth();
+	Vec3f barycentric(Vec3f A, Vec3f B, Vec3f C, Vec3f P);
+	void DrawTriangleByBarycentric(Vec3f* pts, TGAImage& image, TGAColor& color);
+private:
+	float* zbuffer = nullptr;
+};
