c0e8e1bb7afcb4040a53956a00ec0f46595bf6ad
chaoscoder-cn <colordream2023@gmail.com>
2025-03-18 22:22:39 +0800

    feat：绘制透视投影

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # my-tinyrenderer
-软光栅学习，参考：https://github.com/ssloy/tinyrenderer
+软光栅学习，参考：
+* https://github.com/ssloy/tinyrenderer
+* https://github.com/MrZz233/tinyrenderer_notes
--- myTinyRenderer/src/DrawTriangle.cpp
+++ myTinyRenderer/src/DrawTriangle.cpp
@@ -2,32 +2,7 @@
 #include "Log.h"
 #include <algorithm>
 
-void DrawTriangle::DrawTriangleBySimple1( Vec2i p0, Vec2i p1, Vec2i p2, TGAImage& image, TGAColor& color)
-{
-	if (p0.y < p1.y) p0.swap(p1);
-	if (p0.y < p2.y) p0.swap(p2);
-	if (p1.y < p2.y) p1.swap(p2);
-	if (p0.y == p2.y) return;
-	for (int y = p0.y; y >= p2.y; y--)
-	{
-		if (y >= p1.y)
-		{
-			int x1 = std::floor(p0.x + 1.f * (y - p0.y) * (p2.x - p0.x) / (p2.y - p0.y) + 0.5f);
-			int x2 = p1.y == p0.y ? p1.x : std::floor(p0.x + 1.f * (y - p0.y) * (p1.x - p0.x) / (p1.y - p0.y) + 0.5f);
-			if (x1 > x2) std::swap(x1, x2);
-			for (int x = x1; x <= x2; x++)
-				image.set(x, y, color);
-		}
-		else
-		{
-			int x1 = std::floor(p2.x + 1.0 * (y - p2.y) * (p0.x - p2.x) / (p0.y - p2.y) + 0.5f);
-			int x2 = p1.y == p2.y ? p1.x : std::floor(p2.x + 1.0 * (y - p2.y) * (p1.x - p2.x) / (p1.y - p2.y) + 0.5f);
-			if (x1 > x2) std::swap(x1, x2);
-			for (int x = x1; x <= x2; x++)
-				image.set(x, y, color);
-		}
-	}
-}
+
 DrawTriangleByDepth::DrawTriangleByDepth(int image_width, int image_height)
 {
 	zbuffer = new float[image_width * image_height];
