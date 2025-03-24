f3d1c847ff1c071928ee9598ab5c83db2dfd5c0f
chaoscoder-cn <colordream2023@gmail.com>
2025-03-24 22:41:54 +0800

    feat：光照强度不正确

--- myTinyRenderer/src/main.cpp
+++ myTinyRenderer/src/main.cpp
@@ -93,7 +93,6 @@ int main()
 	Matrix Projection = Matrix::identity(4);
 	Matrix ViewPort = viewport(width / 8.0, height / 8.0, width * 3.0 / 4, height * 3.0 / 4);
 
-	//Projection[3][2] = -1.f / camera.z;
 	Projection[3][2] = -1.f / (eye - center).norm();
 
 
@@ -103,12 +102,13 @@ int main()
 
 	Model model(obj_path.data());
 
-	Vec3f light_dir(0, 0, -1);
+	Vec3f light_dir = Vec3f(0, -1, -1).normalize();
 	for (int i = 0; i < model.nfaces(); i++)
 	{
 		std::vector<int>face_i = model.face(i);
 		Vec3f p[3];
 		Vec3f w[3];
+		Vec3f intensity;
 		for (int j = 0; j < 3; j++)
 		{
 			Vec3f v = model.vert(face_i[j]);
@@ -120,12 +120,10 @@ int main()
 
 			//3. Projection to ViewPort
 			p[j] = m2v(ViewPort * Projection * m_v);
+			intensity[j] = model.normal(i, j) * light_dir;
 			
 		}
-		Vec3f n = (w[2] - w[0]) ^ ((w[1] - w[0]));
-		n.normalize();
-		float intensity =  n * light_dir;
-		if (intensity > 0.f)
+		//intensity.normalize();
 		{
 			Vec2i uv[3];
 			for (int j = 0; j < 3; j++) 
@@ -139,81 +137,5 @@ int main()
 	tgaImage.flip_vertically();
 	tgaImage.write_tga_file("obj_4.tga");
 
-	//绘制带有平行光效果的obj
-	/*TGAImage tgaImage(width, height, TGAImage::RGB);
-	DrawTriangle drawTriangle;
-
-	std::string obj_path = "african_head.obj";
-	Model model(obj_path.data());
-
-	Vec3f light_dir(0, 0, -1);
-	for (int i = 0; i < model.nfaces(); i++)
-	{
-		std::vector<int>face_i = model.face(i);
-		Vec2i p[3];
-		Vec3f w[3];
-		for(int j=0;j<3;j++)
-		{
-			float p_x = model.vert(face_i[j]).x;
-			float p_y = model.vert(face_i[j]).y;
-			p[j] = Vec2i{ static_cast<int>((p_x + 1) / 2 * width),static_cast<int>((p_y + 1) / 2 * height) };
-			w[j] = model.vert(face_i[j]);
-		}
-		Vec3f n = (w[2] - w[0]) ^ ((w[1] - w[0]));
-		n.normalize();
-		float intensity = n * light_dir;
-		if (intensity > 0.f)
-		{
-			TGAColor drawColor(255 * intensity, 255 * intensity, 255 * intensity, 255 * intensity);
-			drawTriangle.DrawTriangleBySimple1(p[0], p[1], p[2], tgaImage, drawColor);
-		}
-	}
-
-	tgaImage.flip_vertically();
-	tgaImage.write_tga_file("obj.tga");*/
-	
-
-	//直接绘制obj的所有面
-	/*TGAImage tgaImage(width, height, TGAImage::RGB);
-	DrawTriangle drawTriangle;
-
-	std::string obj_path = "african_head.obj";
-	Model model(obj_path.data());
-
-	Vec3f light_dir(0, 0, -1);
-	for (int i = 0; i < model.nfaces(); i++)
-	{
-		std::vector<int>face_i = model.face(i);
-		Vec2i p[3];
-		Vec3f w[3];
-		for (int j = 0; j < 3; j++)
-		{
-			float p_x = model.vert(face_i[j]).x;
-			float p_y = model.vert(face_i[j]).y;
-			p[j] = Vec2i{ static_cast<int>((p_x + 1) / 2 * width),static_cast<int>((p_y + 1) / 2 * height) };
-		}
-
-		drawTriangle.DrawTriangleBySimple1(p[0], p[1], p[2], tgaImage, white);
-
-	}
-
-	tgaImage.flip_vertically();
-	tgaImage.write_tga_file("obj_1.tga");*/
-
-
-
-	
-	//绘出来的三个三角形
-	/*Vec2i t0[3] = { Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80) };
-	Vec2i t1[3] = { Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180) };
-	Vec2i t2[3] = { Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180) };
-
-	TGAImage image(width, height, TGAImage::RGB);
-	DrawTriangle drawTriangle;
-	drawTriangle.DrawTriangleBySimple1(t0[0], t0[1], t0[2], image, red);
-	drawTriangle.DrawTriangleBySimple1(t1[0], t1[1], t1[2], image, white);
-	drawTriangle.DrawTriangleBySimple1(t2[0], t2[1], t2[2], image, green);
-
-	image.write_tga_file("triangle.tga");*/
 	return 0;
 }
